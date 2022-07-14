5257bfa18a5f46b915b88e2401c01ec97b63fd54
Philip Levy <philipdlevy@gmail.com>
2022-07-14 10:05:27 -0600

    commit tree 15ae6b4925ac0ce36f08ebd986744d1edbd4de5f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ApplicationRecord
+    
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+    validate :clickbait_title
+
+
+    def clickbait_title
+        if title
+            unless title.include? "Won't Believe" || "Secret" || "Top [number]" || "Guess"
+                errors.add(:title, "Not clickbait-y enough!")
+            end
+        end
+    end
+
+    ###refactored
+    # & symbol says, if it exists, THEN run the inlcudes, if not, do nothing.
+    # def clickbait_title
+    #     unless title&.include? "Won't Believe" || "Secret" || "Top [number]" || "Guess"
+    #         errors.add(:title, "Not clickbait-y enough!")
+    #     end
+    # end
 end
